1dba4cffa05ed2eaca8ad2ba69f2c3f3fbb2dbea
Peter Seale <peter@pseale.com>
2023-12-03 13:46:47 -0600

    upgrade from dotnet 7 SDK to dotnet 8 SDK - it's a bit simpler!

--- src/01-BuildingContainers/DONOTUSE_VisualStudioContainerDevelopmentTools/build.ps1
+++ src/01-BuildingContainers/DONOTUSE_VisualStudioContainerDevelopmentTools/build.ps1
@@ -1,9 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-# clear out bin/obj folders - we can't trust dotnet clean
-Remove-Item "$PScriptRoot/bin" -Recurse -Force -ErrorAction 0
-Remove-Item "$PScriptRoot/obj" -Recurse -Force -ErrorAction 0
-
 cd $PSScriptRoot
 # `dotnet publish` is not necessary, because we build in the container!
 # dotnet publish -c Release
--- src/01-BuildingContainers/NoTooling/build.ps1
+++ src/01-BuildingContainers/NoTooling/build.ps1
@@ -1,9 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-# clear out bin/obj folders - we can't trust dotnet clean
-Remove-Item "$PScriptRoot/bin" -Recurse -Force -ErrorAction 0
-Remove-Item "$PScriptRoot/obj" -Recurse -Force -ErrorAction 0
-
 cd $PSScriptRoot
 dotnet publish -c Release --os linux --arch x64
 if (!$?) { throw "error during dotnet publish" }
